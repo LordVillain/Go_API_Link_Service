8bea6440814b70e6818eb096d2471419bbdb7b9b
LordVillain <danyailyinchyk2005@gmail.com>
2025-01-28 00:50:52 +0300

    пофикшен баг с авторизацией

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -31,9 +31,9 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 		EventBus: deps.EventBus,
 	}
 
-	router.HandleFunc("POST /link", handler.Create())
+	router.Handle("POST /link", middleware.IsAuthed(handler.Create(), deps.Config))
 	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update(), deps.Config))
-	router.HandleFunc("DELETE /link/{id}", handler.Delete())
+	router.Handle("DELETE /link/{id}", middleware.IsAuthed(handler.Delete(), deps.Config))
 	router.HandleFunc("GET /{hash}", handler.GoTo())
 	router.Handle("GET /link", middleware.IsAuthed(handler.GetAll(), deps.Config))
 }
